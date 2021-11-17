3ab501091c24a064b90a5602bdb419f50ee8389c
Sheralyn Files <gired0329@yahoo.com>
2021-11-16 20:17:24 -0500

    add schema

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: 'Enter exercise to perform',
+            },
+            name: {
+                type: String,
+                required: 'Enter exercise name',
+            },
+            duration: {
+                type: Number,
+                required: 'Enter length of exercise in minutes',
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+        },
+    ],
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const controllers = require("./controllers");
+const controllers = require("./controllers/api.js");
 
 const PORT = process.env.PORT || 3000;
 
